55e1aaea65e8fcc91ca6400ae07cf6a045841b57
noor <noorsaad18@gmail.com>
2021-02-07 23:47:01 +0300

    rental category

---
+++ lib/Screen/home_tabs/Category_card.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CategoryCard extends StatelessWidget {
+  final Image image;
+  final String name;
+
+  CategoryCard(this.image, this.name);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [BoxShadow(blurRadius: 2, color: Colors.grey)]),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              image,
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                name,
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Color((0xff6e475b)),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/Screen/home_tabs/Rental.dart
+++ lib/Screen/home_tabs/Rental.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Category_card.dart';
 
 class Rental extends StatefulWidget {
   @override
@@ -8,196 +9,122 @@ class Rental extends StatefulWidget {
 class _RentalState extends State<Rental> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          _drawHeader(),
-          _drawTopStories(),
-          _drawMidStories(),
-        ],
-      ),
-    );
-  }
-
-  Widget _drawHeader() {
-    TextStyle _headerTitle = TextStyle(
-      color: Color(0xff925e78),
-      fontSize: 30,
-      fontWeight: FontWeight.w600,
-    );
-    TextStyle _headerDescription = TextStyle(
-      color: Color(0xff925e78),
-      fontSize: 20,
-    );
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height * 0.25,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: ExactAssetImage('images/bg3.png'),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(left: 48, right: 48),
-              child: Text(
-                'Online Rental',
-                style: _headerTitle,
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 36, right: 36),
-              child: Text(
-                'FOR ALL PRDUCTS IN YOUR HOME',
-                style: _headerDescription,
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _drawTopStories() {
-    return Container(
-      color: Colors.white,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(left: 16, top: 16),
-            child: Text(
-              'Top Salary',
-              style: TextStyle(
-                color: Color(0xff925e78),
-                fontWeight: FontWeight.w600,
-                fontSize: 18,
-              ),
-            ),
+    return ListView(
+      //crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height * 0.25,
+          decoration: BoxDecoration(
+            color: Colors.grey[300],
           ),
-          Padding(
-            padding: EdgeInsets.all(8.0),
+          child: Center(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Row(
-                    children: <Widget>[
-                      SizedBox(
-                        child: Image(
-                          image: ExactAssetImage('images/rent.png'),
-                          fit: BoxFit.cover,
-                        ),
-                        width: 121,
-                        height: 121,
-                      ),
-                      SizedBox(
-                        width: 16,
-                      ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              'Party Suppliers',
-                              maxLines: 2,
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 23,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Text('Noor'),
-                                Row(
-                                  children: <Widget>[
-                                    Icon(Icons.timer),
-                                    Text('5 hours'),
-                                  ],
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      ),
-                    ],
+                  padding: const EdgeInsets.only(left: 48, right: 48),
+                  child: Text(
+                    'Online Rental',
+                    style: TextStyle(
+                      color: Color(0xff925e78),
+                      fontSize: 30,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 36, right: 36),
+                  child: Text(
+                    'FOR ALL PRDUCTS IN YOUR HOME',
+                    style: TextStyle(
+                      color: Color(0xff925e78),
+                      fontSize: 20,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
                 ),
               ],
             ),
           ),
-        ],
-      ),
-    );
-  }
-
-  Widget _drawMidStories() {
-    return Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Row(
-              children: <Widget>[
-                SizedBox(
-                  child: Image(
-                    image: ExactAssetImage('images/bg3.png'),
-                    fit: BoxFit.cover,
-                  ),
-                  width: 121,
-                  height: 121,
-                ),
-                SizedBox(
-                  width: 16,
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Electric Machines',
-                        maxLines: 2,
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 23,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text('suzan '),
-                          Row(
-                            children: <Widget>[
-                              Icon(Icons.timer),
-                              Text('5 hours'),
-                            ],
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
+        ),
+        //start
+        Container(
+          padding: EdgeInsets.all(10),
+          child: Text(
+            'Category',
+            style: TextStyle(
+                fontSize: 30,
+                color: Color(0xff925e78),
+                fontWeight: FontWeight.w600),
           ),
-        ],
-      ),
+        ),
+        Container(
+          height: 350,
+          child: GridView(
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            children: [
+              InkWell(
+                onTap: () {},
+                child: GridTile(
+                    child: CategoryCard(
+                        Image.asset(
+                          'images/electronics.png',
+                          width: 110,
+                          height: 110,
+                        ),
+                        'Electronics')),
+              ),
+              InkWell(
+                onTap: () {},
+                child: GridTile(
+                    child: CategoryCard(
+                        Image.asset(
+                          'images/house.png',
+                          width: 110,
+                          height: 110,
+                        ),
+                        'Home')),
+              ),
+              InkWell(
+                onTap: () {},
+                child: GridTile(
+                    child: CategoryCard(
+                        Image.asset(
+                          'images/tools-and-utensils.png',
+                          width: 110,
+                          height: 110,
+                        ),
+                        'Tools')),
+              ),
+              InkWell(
+                onTap: () {},
+                child: GridTile(
+                    child: CategoryCard(
+                        Image.asset(
+                          'images/puzzle.png',
+                          width: 110,
+                          height: 110,
+                        ),
+                        'Games')),
+              ),
+              InkWell(
+                onTap: () {},
+                child: GridTile(
+                    child: CategoryCard(
+                        Image.asset(
+                          'images/other.png',
+                          width: 110,
+                          height: 110,
+                        ),
+                        'Others')),
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
