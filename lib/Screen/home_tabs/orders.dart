cda317083e3344c5f7a7cf96bcc8c5a21b7b4df1
suzan9797 <algarousha.s@gmail.com>
2021-03-02 14:26:43 +0300

    nor

--- lib/Screen/home_tabs/orders.dart
+++ lib/Screen/home_tabs/orders.dart
@@ -58,7 +58,36 @@ class _OrdersState extends State<Orders> {
 }
 
 Widget newRequest(BuildContext context) {
-  return Container();
+  return ListView(
+    children: <Widget>[
+      Padding(
+        padding: const EdgeInsets.only(top: 45, left: 10, right: 10),
+        child: Container(
+          height: 120,
+          width: 150,
+          child: Card(
+            elevation: 14,
+            child: Expanded(
+              child: Container(
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      '  Date and Time:',
+                      style: TextStyle(color: Colors.grey[800], fontSize: 17),
+                    ),
+                    Text(
+                      '  Status of Requsts: ',
+                      style: TextStyle(color: Colors.grey[800], fontSize: 17),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    ],
+  );
 }
 
 Widget oldRequest(BuildContext context) {
